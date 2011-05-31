a83f897284f4dfb988202161632309e169001a96
Sandeep Kolla <kollasandeep@gmail.com>
2011-05-31 18:03:03 -0400

    Sandeep's Work : adding a Project

---
+++ Repo_1/Repo_1/Repo_1AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  Repo_1AppDelegate.h
+//  Repo_1
+//
+//  Created by Sandeep Kolla on 5/31/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Repo_1AppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
